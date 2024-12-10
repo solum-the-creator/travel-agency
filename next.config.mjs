e186da9b7ea4c36c3ad3bd8748b0f58a36807b9f
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-12-10 13:30:09 +0300

    chore: configure base styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    silenceDeprecations: ['legacy-js-api'],
+    additionalData: `$backgroundPrimary: #ffffff;
+                    $backgroundSecondary: #fafafa;
+                    $primary: #7c6a46;
+                    $secondary: #fafafa;
+                    $textPrimary: #1c1c1c;
+                    $textSecondary: #454545;
+                    $textLight: #ffffff;
+                    $accent: #7c6a46;`,
+  },
+};
 
 export default nextConfig;
